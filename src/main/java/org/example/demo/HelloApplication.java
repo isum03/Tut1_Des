7c8fef6fb589550038b64e5d9f5142ef9e5b7b91
isum03 <isump10@gmail.com>
2024-02-16 23:17:01 +0530

    initial commit.

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -10,12 +10,21 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Load login.fxml initially
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 280, 270);
         stage.setTitle("Login Page");
         stage.setScene(scene);
         stage.show();
+    }
 
+    // Method to switch to signup scene
+    public void switchToSignup(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signup.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 450);
+        stage.setTitle("Registration Page");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/org/example/demo/HelloController.java
+++ src/main/java/org/example/demo/HelloController.java
@@ -1,48 +1,15 @@
-package org.example.demo;
+package org.example.blah;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
-
-import static javafx.application.Application.launch;
 
 public class HelloController {
     @FXML
-    private TextField FirstNameTextField;
-    @FXML
-    private TextField  lastnametextfield;
-    @FXML
-    private TextField  agetext;
-    @FXML
-    private DatePicker  birthdaypicker;
-    @FXML
-    private ComboBox<String>genderComboBox;
-    @FXML
-    public void initialize(){
-        genderComboBox.getItems().addAll("Male","Female","Other");
-    }
-   /* @FXML
-    private TextField  agetext;
-    @FXML
-    private TextField  agetext;
-    */
+    private Label welcomeText;
 
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
 }
 
-
-
-
-
-        Scene scene = new Scene(grid, 400, 500);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Personal Details and Login Form");
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {launch();}
-}
